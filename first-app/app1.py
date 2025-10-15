692e759baa79a4e305c90dab8e8428e61b4997c5
unknown <masteravinashrai2@gmail.com>
2025-10-16 01:23:01 +0530

    app1- added streamlit

---
+++ first-app/app1.py
@@ -0,0 +1,19 @@
+import webbrowser
+import urllib.parse
+import pandas as pd
+import streamlit as st
+
+st.title('Employee Hierarchy Visualization')
+df= pd.read_csv("data/employees.csv",header=0).convert_dtypes()
+st.dataframe(df)
+
+edges = ""
+for _, row in df.iterrows():
+  if not pd.isna(row.iloc[1]):
+    edges += f'\t"{row.iloc[0]}" -> "{row.iloc[1]}";\n'
+
+d=f'digraph {{\n{edges}}}'
+st.graphviz_chart(d)
+#url="https://dreampuf.github.io/GraphvizOnline/#" + urllib.parse.quote(d)
+#webbrowser.open(url)
+#print(d)
